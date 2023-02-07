c0c87d71a9b0c839ed7f833f51223b8b81c24526
snafu <aodomleo@gmail.com>
2023-02-07 14:51:02 -0800

    Fourth commit.

--- sorting.cpp
+++ sorting.cpp
@@ -6,17 +6,49 @@
 
 void selectionSort(std::vector<int> sortThis, int nodes) {
 
+int min;
+
+	for(int x=0; x<nodes; x++) {
+	
+		sortThis.at(x) = x;
+		std::cout << sortThis.at(x) << " ";
+
+	}
+
+	for(int i=0; i<nodes; i++) {
+
+		min = i;
+		for(int j=i+1; j<nodes; j++) {
+
+			if(sortThis.at(j) < sortThis.at(min)) {
+
+				std::swap (sortThis.at(i), sortThis.at(min));
+
+			}
+
+		}
+
+	}
+	
+	std::cout << "\n";
+	std::cout << "Sorted: ";
+
+	for(int y=0; y<nodes; y++) {
+
+		sortThis.at(y) = y;
+		std::cout << sortThis.at(y) << " ";
+
+	}
+
+}
+
+void bubbleSort(std::vector<int>, char nodes) {
+
 
 
 }
 
-void bubbleSort(int vector, char nodes) {
-
-
-
-}
-
-void insertionSort(int vector, char nodes) {
+void insertionSort(std::vector<int>, char nodes) {
 
 
 
